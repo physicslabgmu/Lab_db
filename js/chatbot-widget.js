502b2b05e54a46eafa5fdfb561c16a4f8b9e0c91
Lab_db <physicslab.gmu@gmail.com>
2025-02-10 12:19:46 -0500

    Update chatbot-widget.js

--- js/chatbot-widget.js
+++ js/chatbot-widget.js
@@ -29,8 +29,8 @@ document.addEventListener('DOMContentLoaded', function() {
             position: fixed;
             bottom: 20px;
             right: 20px;
-            width: 700px;
-            height: 750px;
+            width: 600px;
+            height: 570px;
             background: white;
             border-radius: 8px;
             box-shadow: 0 0 10px rgba(0,0,0,0.1);
